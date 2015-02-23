542237cc41a35e70b20e3b3ad2dec5c4870c019a
Gant <GantMan@gmail.com>
2015-02-23 09:41:11 -0600

    added the greeting and time to homepage

--- home_splash/app/controllers/home_controller.rb
+++ home_splash/app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
+    right_now = Time.now
+
+    @current_time = right_now.to_formatted_s(:short)
+
+    # Adjust message by time of day
+    @greet_message = case right_now.hour
+    when 5..11
+      "GoOoooOoOoo Mernin!"
+    when 12..15
+      "Happy Afternoon!"
+    when 16..19
+      "Good evening..."
+    when 20..23, 0..4
+      "Good Night Sweetheart"
+    end
+
   end
 end
